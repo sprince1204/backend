deaf0b0368801ca8873bd2c55daeacea532d41c2
Prince Kumar <sprincekumar1204@gmail.com>
2024-10-11 16:37:24 +0530

    Add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 const registerUser = asyncHandler( async (req, res) =>{
     //get user details from frontend
     //validation- not empty
@@ -36,8 +37,32 @@ const registerUser = asyncHandler( async (req, res) =>{
         throw new ApiError(400, "Avatar is required")
     }
 
-    uploadOnCloudinary
+    const avatar =  await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName, 
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const CreatedUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if(!createdUser){
+        throw new ApiError(500, "something went Wrong while creating user")
+    }
+
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered Successfully")
+    )
 
 })
 export {registerUser}
