5bf241c1d789b5d49a48e48e640adaf680254b8b
Prince Kumar <sprincekumar1204@gmail.com>
2024-10-22 01:22:15 +0530

    subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose {Schema} from "mongoose"
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.type.ObjectId, //user who is a subscriber
+        ref: "User"
+    }
+    channel: {
+        type: Schema.type.ObjectId, //user 'subscriber' who is subscribing
+        ref: "User"
+    }
+}, {timestamps: true})
+
+export const Subscription  = mongoose.model("Subscription", subscriptionSchema)
